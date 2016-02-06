779c121b0d39d55cc167a390c2fdef87325aa738
sweetrabh <sweetrabh@gmail.com>
2016-02-05 19:29:44 -0800

    initial commit

---
+++ face/face.py
@@ -0,0 +1,15 @@
+import face.edf as edf
+
+
+# noinspection PyMethodMayBeStatic
+
+
+
+def get_blink_twice_ds():
+    path_blink_twice_csv = "C:/Users/SourabhKatti/Documents/engine/face/edf/converted_csv/blink-twice/blink-twice-1-03.02.16.19.40.57.csv"
+    path_blink_twice_target = "C:/Users/SourabhKatti/Documents/engine/face/edf/converted_csv/blink-twice/blink-twice-1-03.02.16.19.40.57.txt"
+
+    gyro_x, gyro_y, electrodes, sample_numbers, time_ms, time_s = edf.importfile(path_blink_twice_csv)
+    train_y = edf.get_targets_by_ms(path_blink_twice_target, time_ms)
+
+    return [gyro_x, gyro_y, electrodes, sample_numbers, time_ms, time_s], train_y
---
+++ main.py
@@ -0,0 +1,33 @@
+import face.face as face
+
+
+# [gyro_x, gyro_y, electrodes, sample_numbers, time_ms, time_s]
+#     0      1          2             3            4       5
+
+def getdatasets_eeg():
+    eeg_input, output = face.get_blink_twice_ds()
+    return eeg_input[2], output
+
+
+def getdatasets_gyroxy():
+    eeg_input, output = face.get_blink_twice_ds()
+    return eeg_input[0:1], output
+
+
+def getdatasets_samplenums():
+    eeg_input, output = face.get_blink_twice_ds()
+    return eeg_input[3], output
+
+
+def getdatasets_timems():
+    eeg_input, output = face.get_blink_twice_ds()
+    return eeg_input[4], output
+
+
+def getdatasets_times():
+    eeg_input, output = face.get_blink_twice_ds()
+    return eeg_input[5], output
+
+
+eeginput, eegoutput = getdatasets_eeg()
+print(eeginput.shape)
