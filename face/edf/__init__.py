bf4717877a6c70f29ad49264b46f5e6b46299ec7
sweetrabh <sweetrabh@gmail.com>
2016-05-13 17:29:30 -0700

    Add files via upload

---
+++ face/edf/__init__.py
@@ -0,0 +1,154 @@
+import numpy as np
+import os
+import re
+
+
+# sample number = 1
+
+# AF3 = 3
+# F7 = 4
+# F3 = 5
+# FC5 = 6
+# T7 = 7
+# P7 = 8
+# O1 = 9
+# O2 = 10
+# P8 = 11
+# T8 = 12
+# FC6 = 13
+# F4 = 14
+# F8 = 15
+# AF4 = 16
+
+# gyro_x = 17
+# gyro_y = 18
+
+# time_s = 21
+# time_ms = 22
+
+# 174 317
+
+
+## import data from an csv file.
+# Input takes in a path to a directory of csv files
+def importfolder(path, count):
+    raw_edfs = []
+    gyro_x = []
+    gyro_y = []
+    electrodes = []
+    sample_numbers = []
+    time_ms = []
+    time_s = []
+
+    print(path)
+
+    # Return if there are less files in the folder than the requested count
+    files = os.listdir(path)
+    if files.__len__() < count:
+        print("No files found")
+        return
+
+    for file in files:
+        path_to_file = path + '/' + file
+        csvmatch = re.search("csv", path_to_file)
+        if csvmatch:
+            print(path_to_file)
+            raw_edf = np.loadtxt(path_to_file, dtype=str, delimiter=',', skiprows=1)
+            raw_edfs.append(raw_edf)
+
+
+def importfile(path):
+    raw_edfs = []
+    gyro_x = []
+    gyro_y = []
+    electrodes = []
+    sample_numbers = []
+    time_ms = []
+    time_s = []
+
+    raw_edf = np.loadtxt(path, dtype=str, delimiter=', ', skiprows=1)
+    sanitized_edf = sanitizeedf(raw_edf)
+
+    electrodes = sanitized_edf[:, 2:16]
+
+    sample_numbers = sanitized_edf[:, 0]
+
+    time_ms = sanitized_edf[:, 22]
+    time_s = sanitized_edf[:, 21]
+
+    gyro_x = sanitized_edf[:, 17]
+    gyro_y = sanitized_edf[:, 18]
+
+    return gyro_x, gyro_y, electrodes, sample_numbers, time_ms, time_s
+
+
+# Clean out the special characters in an edf and convert to float
+def sanitizeedf(raw_edf):
+    raw_edf = np.asarray(raw_edf)
+    sanitized_edf = np.zeros(raw_edf.shape, dtype=float)
+    sample_index = 0
+    for sample in raw_edf:
+        sanitized_sample = np.zeros(sample.shape, dtype=float)
+        channel_index = 0
+        for channel in sample:
+            sanitized_sample[channel_index] = np.asarray(re.sub("[a-z\']", string=channel, repl="")).astype(float)
+            channel_index += 1
+        sanitized_edf[sample_index] = sanitized_sample
+        sample_index += 1
+    return sanitized_edf
+
+
+# Return a list of targets based on a provided text file
+def get_targets_by_ms(path_target, time_ms):
+
+    # Load up values of ms to compare to
+    target_counts = np.loadtxt(path_target, dtype=int, delimiter=' ')
+
+    target_shape = target_counts.shape
+
+    samples = time_ms.__len__()
+    print("Total number of samples: ", samples)
+
+
+    ms_sum = 0
+    ms_index = 0
+
+    target_output = np.zeros(samples)
+
+    print("Shape of target values: ", target_shape)
+
+    # For each sample in time_ms array, write a 0 as long as the target_counts have not been reached.
+    # If a target count is reached, write a 1 for the next sample
+
+    # Add a 0 to help the array sizes match up later
+    p = 0
+    target_added = False
+    while p < target_shape[0]:
+        target = target_counts[p][2]
+        min_ms = target_counts[p][0]
+        max_ms = target_counts[p][1]
+
+        while (not target_added) and (ms_index < samples):
+            ms_diff = time_ms[ms_index + 1] - time_ms[ms_index]
+
+            # Check if the next time is from a new second. Adjust the difference if it is.
+            if ms_diff < 0:
+                ms_diff += 1000
+
+
+            # Add the time difference to the ongoing sum
+            ms_sum += ms_diff
+            ms_index += 1
+
+            # Check if the maximum time stamp has been passed for this target.
+            # If it isn't, add the target value to this index
+            if ms_sum > max_ms:
+                break
+
+            elif ms_sum > min_ms:
+                target_output[ms_index] = target
+
+        p += 1
+    return np.asarray(target_output).astype(float)
+
+
